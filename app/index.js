dc233077717b290924effb0cef4ce969ef041e3f
Simon Mai <maismin@gmail.com>
2019-10-09 02:40:20 -0400

    Add light/dark theme

--- app/index.js
+++ app/index.js
@@ -4,18 +4,41 @@ import './index.css';
 
 import Battle from './components/Battle';
 import Popular from './components/Popular';
+import Nav from './components/Nav';
+
+import { ThemeProvider } from './contexts/theme';
 
 // Component
 // State
 // Lifecycle
 // UI
 
+/* eslint-disable react/no-unused-state */
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light',
+        }));
+      },
+    };
+  }
+
   render() {
+    const { theme } = this.state;
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={theme}>
+          <div className="container">
+            <Nav />
+            <Popular />
+          </div>
+        </div>
+      </ThemeProvider>
     );
   }
 }
