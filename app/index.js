c16fc5b80ce091f1d6971b5f02b6a5a7f0ae0fb1
Simon Mai <maismin@gmail.com>
2019-10-09 11:12:43 -0400

    Add 404

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 
 import Battle from './components/Battle';
@@ -38,9 +38,12 @@ class App extends React.Component {
           <div className={theme}>
             <div className="container">
               <Nav />
-              <Route exact path="/" component={Popular} />
-              <Route exact path="/battle" component={Battle} />
-              <Route path="/battle/results" component={Results} />
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
             </div>
           </div>
         </ThemeProvider>
